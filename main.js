7bea45d33cc767837e57b489fd90b8b37c9eb58c
Justin Visser <justin@jhvisser.com>
2013-12-08 01:35:14 -0500

    Adding some comments

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+//Title: Facebook Autopoke-back
+//Author: Justin V
+//Website: http://jhvisser.com
+//Description: Automatically pokes back a user when they poke you.
+
+//Main pokeback function.
 var pokeBack = function(){
 	if($('#content a:contains("Poke back")').length > 0) {
-		console.log('Scanning for pokes...');
+		console.log('Poke found! Attempting to poke back...');
 		$('#content a:contains("Poke back")')[0].click();
 	}
 }
+//Sets pokeback check to happen every 7.5 seconds.
 setInterval(pokeBack,7500);
